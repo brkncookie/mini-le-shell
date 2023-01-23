096a628a4fe1deb8cbfd0291d5789fe772b32cf6
mnadir <mnadir@student.1337.ma>
2023-01-23 15:21:01 +0100

    on the way to the parser

--- include/parser.h
+++ include/parser.h
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnadir <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/23 13:23:15 by mnadir            #+#    #+#             */
+/*   Updated: 2023/01/23 13:27:08 by mnadir           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #ifndef PARSER_H
 # define PARSER_H
 
+# include "lexer.h"
+
 typedef struct s_tree
 {
-	t_tkn	*tkn;
+	t_tkns	*tkn;
 	t_tree	*limn;
 	t_tree	*lisr;
-} t_tree;
+}		t_tree;
 #endif
-
