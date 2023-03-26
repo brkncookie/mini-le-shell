76e2f6f5caa2951d3fdf83d89da4e49ab5a78396
brkncookie <the0007end@gmail.com>
2023-03-26 23:37:39 +0000

    fixed some error checking in do_lqados and shortened the funtions lines

--- src/executor.c
+++ src/executor.c
@@ -64,22 +64,31 @@ int	do_builtin(t_tree *cmdtree, int *redr_fds, t_list **vars_lst)
 		return ((dup2(in, 0), dup2(out, 1)), 0);
 }
 
+int	do_child(char	*prgm, t_tree	*cmdtree, t_list **vars_lst, int *redr_fds)
+{
+	char	**envs;
+
+	envs = get_dblarr(vars_lst);
+	prgm = is_vld_exc(prgm, vars_lst, NULL, NULL);
+	if (!prgm)
+		exit(127);
+	if (dup2(redr_fds[0], 0) < 0 || dup2(redr_fds[1], 1) < 0)
+		return (perror("dup2"), exit(errno), 1);
+	execve(prgm, cmdtree->arg, envs);
+	return (free_dblarr(envs, ft_lstsize(*vars_lst)), \
+		perror("execve"), exit(errno), 1);
+}
+
 int	do_cmd(t_tree *cmdtree, int *redr_fds, int limn, t_list **vars_lst)
 {
 	int		r_val;
 	int		pid;
 	char	*prgm;
-	char	**envs;
 
-	envs = get_dblarr(vars_lst);
 	if ((cmdtree->tkn->type & (REDR_I | REDR_O | APPEND | HERE_DOC)))
 		return (rslv_redr(cmdtree, redr_fds, 0, 1), errno);
-	if (cmdtree->redr)
-	{
-		redr_fds = rslv_redr(cmdtree->redr, redr_fds, 0, 1);
-		if (!redr_fds)
-			return (errno);
-	}
+	if (cmdtree->redr && !rslv_redr(cmdtree->redr, redr_fds, 0, 1))
+		return (errno);
 	if (do_builtin(cmdtree, redr_fds, vars_lst))
 		return (pipe_close(redr_fds, limn), g_flag);
 	prgm = ft_strndup(cmdtree->arg[0], ft_strlen(cmdtree->arg[0]));
@@ -89,22 +98,11 @@ int	do_cmd(t_tree *cmdtree, int *redr_fds, int limn, t_list **vars_lst)
 	if (pid < 0)
 		return (perror("fork"), g_flag = 254);
 	if (!pid)
-	{
-		prgm = is_vld_exc(prgm, vars_lst);
-		if (!prgm)
-			exit(127);
-		if (redr_fds)
-			if (dup2(redr_fds[0], 0) < 0 || dup2(redr_fds[1], 1) < 0)
-				return (perror("dup2"), exit(errno), 1);
-		execve(prgm, cmdtree->arg, envs);
-		return (perror("execve"), exit(errno), 1);
-	}
+		do_child(prgm, cmdtree, vars_lst, redr_fds);
 	pipe_close(redr_fds, limn);
 	if (limn > 0 || limn == -2)
 		waitpid(pid, &r_val, 0);
-	g_flag = WEXITSTATUS(r_val);
-	free_dblarr(envs, ft_lstsize(*vars_lst));
-	return (free(prgm), WEXITSTATUS(r_val));
+	return (g_flag = WEXITSTATUS(r_val), free(prgm), WEXITSTATUS(r_val));
 }
 
 int	do_lqados(t_tree *cmdtree, int *redr_fds, int limn, t_list **vars_lst)
@@ -114,25 +112,23 @@ int	do_lqados(t_tree *cmdtree, int *redr_fds, int limn, t_list **vars_lst)
 	int	pipefd[2];
 	int	r_val;
 
-	lisr_fds[0] = redr_fds[0];
-	lisr_fds[1] = 1;
-	limn_fds[0] = 0;
-	limn_fds[1] = redr_fds[1];
 	if (!(cmdtree->tkn->type & PIPE))
 		return (do_cmd(cmdtree, redr_fds, limn, vars_lst));
 	if (pipe(pipefd) < 0)
 		return (perror("pipe"), 1);
+	lisr_fds[0] = redr_fds[0];
 	lisr_fds[1] = pipefd[1];
 	limn_fds[0] = pipefd[0];
-	if (cmdtree->redr && rslv_redr(cmdtree->redr, &lisr_fds[0], 0, 0))
-		rslv_redr(cmdtree->redr, &limn_fds[0], 1, 0);
+	limn_fds[1] = redr_fds[1];
+	if (cmdtree->redr && (!rslv_redr(cmdtree->redr, &lisr_fds[0], 0, 0) \
+				|| !rslv_redr(cmdtree->redr, &limn_fds[0], 1, 0)))
+		return (errno);
 	if (cmdtree->lisr->tkn->type & (VAR | WORD))
 		do_cmd(cmdtree->lisr, &lisr_fds[0], -1, vars_lst);
 	else
 		do_logops(cmdtree->lisr, &lisr_fds[0], 1, vars_lst);
 	r_val = do_logops(cmdtree->limn, &limn_fds[0], 1, vars_lst);
-	if (cmdtree->lisr->tkn->type & (VAR | WORD))
-		waitpid(-1, NULL, 0);
+	waitpid(-1, NULL, WNOHANG * !(cmdtree->lisr->tkn->type & (VAR | WORD)));
 	return (r_val);
 }
 
